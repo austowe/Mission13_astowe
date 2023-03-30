8f08214636f94a6c5360c7588e1876cb87265c53
Austin Stowe <austinrstowe@gmail.com>
2023-03-29 21:17:34 -0600

    Updates

---
+++ src/movies.tsx
@@ -0,0 +1,37 @@
+import data from './MovieData.json';
+
+const MDS = data.MovieData;
+
+function MovieData() {
+  return (
+    <>
+      <div>
+        <h3>Joel Hilton's Movie Collection</h3>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Year</th>
+              <th>Director</th>
+              <th>Rating</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {MDS.map((m) => (
+              <tr>
+                <td>{m.Title}</td>
+                <td>{m.Year}</td>
+                <td>{m.Director}</td>
+                <td>{m.Rating}</td>
+                <td>{m.Category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
